2cfdf197d84deffbf1fdc4fe003eacb8ba2aade3
Adolfo <adolfo.brunno.ba@gmail.com>
2015-09-21 15:20:09 -0300

    integracao com saucelabs
    Signed-off-by: Adolfo <adolfo.brunno.ba@gmail.com>

--- config/passport.js
+++ config/passport.js
@@ -2,14 +2,15 @@ var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
 var mongoose = require('mongoose');
 var FacebookStrategy = require('passport-facebook').Strategy;
+var config = require('./config')();
 
 module.exports = function(){
 
 	var Usuario = mongoose.model('Usuario');
 
 	passport.use(new FacebookStrategy({
-		clientID: '1584903918437641',
-	    clientSecret: '93e9ccfff177b53293f551965382a4de',
+		clientID: config.facebookClientID,
+	    clientSecret: config.facebookClientSecret,
 	    callbackURL: "http://localhost:3000/auth/facebook/callback",
 	    enableProof: false
 	}, function(accessToken, refreshToken, profile, done){
@@ -31,8 +32,8 @@ module.exports = function(){
 	}));
 
 	passport.use(new GitHubStrategy({
-		clientID: '917d293b6ba7f6994751',
-		clientSecret: '94c6fe6b1493e1230b68fa4f7f0ecab64af12801',
+		clientID: config.githubClientID,
+		clientSecret: config.githubClientSecret,
 		callbackURL: 'http://localhost:3000/auth/github/callback'
 	}, function(accessToken, refreshToken, profile, done){
 		console.log('------------------------');
